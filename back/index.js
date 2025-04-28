bd4622b2d0431a9cb9035e7da0bbaef7bda9f614
AgusBlanco <agustinanahirblanco@gmail.com>
2025-04-28 17:59:20 -0300

    Regresando a la normalidad las configuraciones

--- back/index.js
+++ back/index.js
@@ -1,11 +1,11 @@
 const app = require("./src/server");
 const conDB = require("./src/config/conDB");
-const updateMongoWhiteList = require("./src/utils/updateMongoWhiteList")
+
+
 const PORT = process.env.PORT || 3000;
 
 async function startServer() {
     try {
-        await updateMongoWhiteList();
         await conDB();
         app.listen(PORT, () => {
             console.log("El servidor se está ejecutando en el puerto 3000")
