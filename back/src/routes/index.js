0f37dbecafec3381d71a40a220f7304a0863942e
AgusBlanco <agustinanahirblanco@gmail.com>
2025-04-29 11:52:07 -0300

    Modificaciones para respuesta en página principal

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -5,6 +5,7 @@ const moviesRouter = require("./moviesRouter");
 
 const router = Router();
 
+router.get("/", (req, res) => res.send("Hello World!"))
 router.use("/movies", moviesRouter);
 
 module.exports = router;
